c36ad3c74d36ada60a65280061a8938b4c5d61b9
Thamy.correa <thamiris_oliveira2@hotmail.com>
2025-02-11 19:29:48 -0300

    Criado arquivo DTO

--- src/main/java/com/thamirisoc/AppContatos/model/MalaDireta.java
+++ src/main/java/com/thamirisoc/AppContatos/model/MalaDireta.java
@@ -1,5 +1,24 @@
 package com.thamirisoc.AppContatos.model;
 
-public class MalaDireta {
 
+public class MalaDireta {
+	
+	private String endereco;		
+	private Integer cep;
+	private String cidade;
+	private String uf;
+	
+	
+	@Override
+	public String toString() {
+		return    "MalaDireta [Rua " + endereco 
+				+ " - CEP: "         + cep      
+				+ " - "              + cidade   
+				+ "/"                + uf       
+				+ "]" 
+				;}
+	
+	
 }
+
+	
