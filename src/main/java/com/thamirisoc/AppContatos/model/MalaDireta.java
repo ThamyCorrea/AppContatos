81f64f69ee5e4529a362a7d5d3faf55d923081e0
Thamy.correa <thamiris_oliveira2@hotmail.com>
2025-02-12 17:22:36 -0300

    Troca a classe MalaDireta de DTO para Record

--- src/main/java/com/thamirisoc/AppContatos/model/MalaDireta.java
+++ src/main/java/com/thamirisoc/AppContatos/model/MalaDireta.java
@@ -1,23 +1,10 @@
 package com.thamirisoc.AppContatos.model;
 
 
-public class MalaDireta {
-	
-	private String endereco;		
-	private Integer cep;
-	private String cidade;
-	private String uf;
-	
-	
-	@Override
-	public String toString() {
-		return    "MalaDireta [Rua " + endereco 
-				+ " - CEP: "         + cep      
-				+ " - "              + cidade   
-				+ "/"                + uf       
-				+ "]" 
-				;}
-	
+public record MalaDireta (String endereco, 
+						  Integer cep, 
+						  String cidade, 
+						  String uf) {
 	
 }
 
