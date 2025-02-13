12214f2eefdf6c2a2b7ae85ff27b3dc0c57d1889
Thamy.correa <thamiris_oliveira2@hotmail.com>
2025-02-12 21:14:34 -0300

    Primeira tentativa de utilizar MalaDireta no getAll, que não deu certo, tentar amanhã

--- src/main/java/com/thamirisoc/AppContatos/model/MalaDireta.java
+++ src/main/java/com/thamirisoc/AppContatos/model/MalaDireta.java
@@ -1,10 +1,26 @@
 package com.thamirisoc.AppContatos.model;
 
 
-public record MalaDireta (String endereco, 
-						  Integer cep, 
+public record MalaDireta (String id,
+						  String nome,
+						  String endereco, 
+						  String cep, 
 						  String cidade, 
 						  String uf) {
+
+	@Override
+	public String toString() {
+		return "id:" + id 
+				+ "\n" 
+				+ "nome:" + nome 
+				+ "\n" 
+				+ "MalaDireta: " 
+				+ "endereco=" + endereco 
+				+ ", cep=" + cep 
+				+ ", cidade=" + cidade + ", uf=" + uf;
+	}
+	
+	
 	
 }
 
