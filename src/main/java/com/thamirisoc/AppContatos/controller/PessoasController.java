c44f59d9778fa94e27c9262e6eebae4b832ec093
Thamy.correa <thamiris_oliveira2@hotmail.com>
2025-02-12 21:12:31 -0300

    Realizado os verbos POST e GET por id em servico e controller

--- src/main/java/com/thamirisoc/AppContatos/controller/PessoasController.java
+++ src/main/java/com/thamirisoc/AppContatos/controller/PessoasController.java
@@ -1,5 +1,50 @@
 package com.thamirisoc.AppContatos.controller;
 
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thamirisoc.AppContatos.model.Pessoas;
+import com.thamirisoc.AppContatos.service.PessoasService;
+
+@RestController
+@RequestMapping("api/pessoas")
 public class PessoasController {
 
+	@Autowired
+	PessoasService pessoaService;
+	
+	@PostMapping
+	public ResponseEntity<Pessoas> save(@RequestBody Pessoas pessoa){
+		Pessoas newPessoa = pessoaService.save(pessoa);
+		
+		if(newPessoa == null) {
+			return ResponseEntity.badRequest().build();
+		}else {
+			return ResponseEntity.ok(newPessoa);
+		}
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Optional<Pessoas>> findById(@PathVariable long id){
+		Optional<Pessoas> pessoa = pessoaService.findById(id);
+		if(pessoa.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}else {
+			return ResponseEntity.ok(pessoa);
+		}
+	}
+	
+	
+	
+	
 }
--- src/main/java/com/thamirisoc/AppContatos/service/PessoasService.java
+++ src/main/java/com/thamirisoc/AppContatos/service/PessoasService.java
@@ -1,5 +1,48 @@
 package com.thamirisoc.AppContatos.service;
 
-public class PessoasService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thamirisoc.AppContatos.model.Pessoas;
+import com.thamirisoc.AppContatos.model.MalaDireta;
+import com.thamirisoc.AppContatos.repository.PessoasRepository;
+
+@Service
+public class PessoasService {
+	
+	@Autowired
+	PessoasRepository pessoasRepository;	
+		
+	public Pessoas save(Pessoas pessoa) {
+		
+		if(pessoa.getNome() == null) {
+			System.out.println("Nome precisa ser preenchido");
+			return null;
+		}
+		
+		try {			
+			return pessoasRepository.save(pessoa);
+			
+		}catch(Exception e) {
+			System.err.println("Erro ao inserir dados pessoais!" +
+							pessoa.toString() + ": " + e.getMessage());
+			return null;
+		}
+		
+	}
+	
+	
+	public Optional<Pessoas> findById(Long id){
+		return pessoasRepository.findById(id);
+	}
+	
+	public List<MalaDireta> listarUsuarios() {
+		List<MalaDireta> pessoas = new ArrayList<>();
+        return pessoas;
+    }
 }
+
